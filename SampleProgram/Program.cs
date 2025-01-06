260e9c938bb631b9ac599179966d7267342f07c6
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-01-06 21:37:26 +0900

    Added and ran sample program.

---
+++ SampleProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using ReadFileLinesEnumGenerator.Generated;
+
+namespace ConsoleApp1;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        foreach (var value in Enum.GetValues<valid>()) Console.WriteLine(value.ToStringValue());
+    }
+}
