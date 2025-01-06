c2b62b4197797e63e3ffacf0f300772b500754fe
kamome283 <26428614+kamome283@users.noreply.github.com>
2025-01-06 21:50:00 +0900

    Added brief tests and checked to pass all cases.

---
+++ ReadFileLinesEnumGenerator.Tests/GeneratorTest.cs
@@ -0,0 +1,25 @@
+﻿using ReadFileLinesEnumGenerator.Generated;
+
+namespace ReadFileLinesEnumGenerator.Test;
+
+public class GeneratorTest
+{
+    [Fact]
+    public void GeneratedEnumValuesLengthIsCorrect()
+    {
+        var values = Enum.GetValues<valid>();
+        Assert.Equal(4, values.Length);
+    }
+
+    [Fact]
+    public void GeneratedEnumValuesStringValuesAreCorrect()
+    {
+        var stringValues = Enum.GetValues<valid>().Select(v => v.ToStringValue());
+        Assert.Equal([
+            "FOO",
+            "bar",
+            "baz.p",
+            "42.x"
+        ], stringValues);
+    }
+}
